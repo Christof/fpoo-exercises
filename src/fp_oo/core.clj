2119b62ede9f6d832f88365942fe53162006be38
Christof Sirk <christof.sirk@gmail.com>
2013-12-29 11:21:06 +0100

    First two exercises.

--- src/fp_oo/core.clj
+++ src/fp_oo/core.clj
@@ -1,6 +1,23 @@
 (ns fp-oo.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Exercise 1
+(defn second [list]
+  (first (rest list)))
+
+(second [1 2 3 4])
+
+; Exercise 2
+(defn third [list]
+  (nth list 2))
+
+(third [1 2 3 4])
+
+(defn third2 [list]
+  (first (rest (rest list))))
+
+(third2 [1 2 3 4])
+
+(defn third3 [list]
+  (second (rest list)))
+
+(third3 [1 2 3 4])
