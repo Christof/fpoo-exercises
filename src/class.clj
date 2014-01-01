2b57647378a6d6a389ae8bb1b60d4a5041057533
Christof Sirk <christof.sirk@gmail.com>
2014-01-01 16:13:44 +0100

    Exercise 1 in 5.4.

--- src/class.clj
+++ src/class.clj
@@ -1,18 +1,21 @@
+(defn method-from-message [class message]
+  (message (:__instance_methods__ class)))
+
+(defn class-from-instance [instance]
+  (eval (:__class_symbol__ instance)))
+
 (defn apply-message-to [class instance message args]
-  (let [method (message (:__instance_methods__ class))]
-    (apply method instance args)))
+  (apply (method-from-message class message)instance args))
 
 (def make
      (fn [class & args]
-       (let [seeded {:__class_symbol__ (:__own_symbol__ class)}
-             constructor  (:add-instance-values (:__instance_methods__ class))]
-         (apply constructor seeded args))))
+       (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+         (apply-message-to class seeded :add-instance-values args))))
 
 (def send-to
      (fn [instance message & args]
-       (let [class (eval (:__class_symbol__ instance))
-             method (message (:__instance_methods__ class))]
-         (apply method instance args))))
+       (let [class (class-from-instance instance)]
+         (apply-message-to class instance message args))))
 
 
 (def Point
