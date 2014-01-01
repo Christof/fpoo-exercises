c7b575d7161af194d1dfdc9e0168df8a77af88ec
Christof Sirk <christof.sirk@gmail.com>
2014-01-01 13:30:36 +0100

    Exercise 4.1.

--- src/embedded-functions.clj
+++ src/embedded-functions.clj
@@ -13,8 +13,17 @@
         :__class_symbol__ 'Point
         :__methods__ {
            :class :__class_symbol__
+           :x :x
+           :y :y
+           :add (fn [this other]
+                    (send-to this :shift (send-to other :x) (send-to other :y)))
            :shift (fn [this xinc yinc]
-                    (make Point (+ (:x this) xinc)
-                                (+ (:y this) yinc)))}}))
+                    (make Point (+ (send-to this :x) xinc)
+                                (+ (send-to this :y) yinc)))}}))
 
 ; Exercise 4.1: x and y getters, use them in shift, implement add
+(def p1 (make Point 1 2))
+(def p2 (make Point 3 4))
+(def sum (send-to p1 :add p2))
+(prn (send-to sum :x))
+(prn (send-to sum :y))
