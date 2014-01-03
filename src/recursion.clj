47a62851613e341e63d688b8eb5d4ed6d48e5f5f
Christof Sirk <christof.sirk@gmail.com>
2014-01-03 19:53:52 +0100

    Exercises 2 and 4 of recursion chapter.

--- src/recursion.clj
+++ src/recursion.clj
@@ -5,3 +5,18 @@
     (* n (factorial (dec n)))))
 
 (factorial 4)
+
+; Exercise 2
+(defn factorial2 [n so-far]
+  (if (= n 1)
+    so-far
+    (factorial2 (dec n) (* so-far n))))
+(prn (factorial2 4 1))
+
+; Exercise 3
+(defn recursive-function [elements so-far]
+  (if (empty? elements)
+    so-far
+    (recursive-function (rest elements) (+ so-far (first elements)))))
+(prn (recursive-function [1 2 3 4] 0))
+
