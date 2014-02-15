9f8fdd7bddcb0c42494517f3e7e5a38e6f9edc4d
Christof Sirk <christof.sirk@gmail.com>
2014-02-15 18:53:57 +0100

    Exercises 4 to 6 for 9.4.

--- src/higher-order-functions.clj
+++ src/higher-order-functions.clj
@@ -16,3 +16,23 @@
     (fn [] x)))
 x ; should produce an error, but nothing happens
 (myfun) ; 3
+
+; Exercise 4
+(def myfun2
+  ((fn [x]
+     (fn [] x)
+     ) 3))
+(myfun2)
+
+; Exercise 5
+(def my-atom (atom 0))
+(deref my-atom)
+(swap! my-atom (fn [_] 33))
+(deref my-atom)
+
+; Exercise 6
+(defn always [value]
+  (fn [& args] value))
+((always 8) 1 'a :foo)
+(swap! my-atom (always 34))
+(deref my-atom)
