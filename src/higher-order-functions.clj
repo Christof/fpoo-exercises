9ae8eb1728a3f00da53d8ff9c6c5a064067b8786
Christof Sirk <christof.sirk@gmail.com>
2014-02-16 11:32:44 +0100

    Exercises 7 to 10 of 9.4.

--- src/higher-order-functions.clj
+++ src/higher-order-functions.clj
@@ -36,3 +36,60 @@ x ; should produce an error, but nothing happens
 ((always 8) 1 'a :foo)
 (swap! my-atom (always 34))
 (deref my-atom)
+
+; Exercise 7
+(defn check-sum [list]
+    (apply + (map * (range 1 (inc (count list))) list)))
+(check-sum [4 8 9 3 2])
+
+; Exercise 8
+(def reversed-digits
+     (fn [string]
+       (map (fn [digit-char]
+              (-> digit-char str Integer.))
+            (reverse string))))
+
+(defn isbn? [string]
+  (let [sum (check-sum (reversed-digits string))]
+    (zero? (rem sum 11))))
+(isbn? "0131774115")
+(isbn? "0977716614")
+(isbn? "1934356190")
+
+; Exercise 9
+(defn check-sum [list]
+  (apply + (map * list (cycle '(1 3)))))
+(check-sum [4 8 9 3 2])
+
+(defn rem-of [num]
+  (fn [n] (rem n num))
+  )
+(defn upc? [string]
+  (-> string
+      reversed-digits
+      check-sum
+      (#(rem % 10))
+      zero?
+      )
+  )
+
+((rem-of 10) 11)
+
+(upc? "074182265830")
+(upc? "731124100023")
+(upc? "722252601404") ;; This one is incorrect.
+
+; Exercise 10
+(defn check-sum [multipliers seq]
+  (apply + (map * seq (multipliers seq)))
+  )
+
+(defn number-checker [divisor multipliers]
+  (fn [string]
+    (-> string
+       reversed-digits
+       (check-sum multipliers)
+       (#(rem % divisor))
+       zero?)))
+(def upc? (number-checker 10 (fn [_] (cycle '(1 3)))))
+(def isbn? (number-checker 11 #(range 1 (inc (count %)))))
