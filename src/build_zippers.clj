b9635b86a405138c44e85f12d09f78c19ea8a626
Christof Sirk <christof.sirk@gmail.com>
2014-03-01 21:04:09 +0100

    Exercise 2 of 11.10.

--- src/build_zippers.clj
+++ src/build_zippers.clj
@@ -23,3 +23,47 @@
     zdown
     zdown
     znode) ; +
+
+; Exercise 2
+(defn seq-zip [tree]
+  {:here tree
+   :parents '() })
+
+(defn zdown [zip]
+  (if (empty? (:here zip))
+    nil
+    (assoc zip
+           :here (first (:here zip))
+           :parents (cons zip (:here zip)))))
+
+(defn znode [zip]
+  (:here zip))
+
+(defn zup [zip]
+  (if (empty? (:parents zip))
+    nil
+    (first (:parents zip))))
+
+
+(-> '(a b c)
+    seq-zip
+    zdown
+    zup
+    znode) ; (a b c)
+
+(-> '(a b c) seq-zip zup) ; nil
+(-> '() seq-zip zdown) ; nil
+
+(defn zroot [zip]
+  (if (empty? (:parents zip))
+    (znode zip)
+    (zroot (zup zip)))
+  )
+
+(-> '(((a)) b c)
+    seq-zip
+    zdown
+    zdown
+    zdown
+    zroot
+    )
