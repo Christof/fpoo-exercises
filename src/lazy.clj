004b614125dd0ca4d38f5e0fe210068073a3e481
Christof Sirk <christof.sirk@gmail.com>
2014-03-08 11:11:07 +0100

    Exercise 1 of 12.4.

---
+++ src/lazy.clj
@@ -0,0 +1,40 @@
+(ns lazy)
+(def rrange
+     (fn [first past-end]
+       (new clojure.lang.LazySeq
+            (fn []
+              (if (= first past-end)
+                nil
+                (cons first
+                      (rrange (inc first) past-end)))))))
+
+; Exercise 1
+(defn mapr [f s]
+  (new clojure.lang.LazySeq
+       (fn []
+         (if (empty? s)
+           nil
+           (cons (f (first s)) (mapr f (rest s)))))))
+
+(mapr inc [1 2 3 4])
+
+;;; For exercise 3.
+
+;;; This throws an exception if the last element of a sequence is
+;;; evaluated when only the `first` is asked for. It uses a large
+;;; source sequence because many builtin sequence functions precalculate
+;;; more than one element when asked for the first. Therefore, if 
+;;; the source sequence were something like [1 2], `filter` would count
+;;; as eager. The number of elements precalculated is subject to change, but
+;;; it's awfully unlikely to be 10,000.
+
+(def eager?
+     (fn [filter-function]
+       (try
+         (not (first (filter-function (fn [elt]
+                                        (if (= elt 9999)
+                                          (throw (Error.)))
+                                        true)
+                                      (range 10000))))
+         (catch Error e
+           true))))
